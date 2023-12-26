ea667ae35e180d19ed8e66a18408cfe97e59b889
D11116126 <154297685+D11116126@users.noreply.github.com>
2023-12-26 11:35:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,25 @@
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const playerDisplay = document.querySelector('#player')
 
-const keys = document.querySelectorAll('.key')
-const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
-const finger = document.querySelector(".finger");
+    let currentPlayer = 'playerX'
 
-keys.forEach(key => {
-  key.addEventListener('click',
-  () => { playNote(key);
-          showFinger(key);})
+    squares.forEach(square => {
+        square.addEventListener('click', clickOutcome)
+    })
 
-  key.addEventListener("mouseup", function () {
-            hideFinger(); // 隱藏手指頭
-          });
-        })
+    function clickOutcome(e) {
+        const squareArray = Array.from(squares)
+        const index = squareArray.indexOf(e.target)
+        playerDisplay.innerHTML = currentPlayer
 
-// document.addEventListener('keydown', e => {
-//   if (e.repeat) return
-//   const key = e.key
-//   const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-//   const blackKeyIndex = BLACK_KEYS.indexOf(key)
+        if (currentPlayer === 'playerX') {
+            squares[index].classList.add('playerX')
+            currentPlayer = 'playerO'
+        } else {
+            squares[index].classList.add('playerO')
+            currentPlayer = 'playerX'
+        }
+    }
 
-//   if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
-//   if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
-// })
-
-function playNote(key) {
-  const noteAudio = document.getElementById(key.dataset.note)
-  noteAudio.currentTime = 0
-  noteAudio.play()
-  key.classList.add('active')
-  noteAudio.addEventListener('ended', () => {
-    key.classList.remove('active')
-  })
-}
-
-function showFinger(key) {
-  const keyRect = key.getBoundingClientRect();
-  finger.style.left = keyRect.left + keyRect.width / 2 - 25 + "px";
-  finger.style.top = keyRect.bottom + "px";
-  finger.style.display = "block";
-}
-
-function hideFinger() {
-  finger.style.display = "none";
-}
+})
